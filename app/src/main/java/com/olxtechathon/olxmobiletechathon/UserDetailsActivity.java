2cd958d4a4e3dd89e502e376dd95276ec80d2877
Surbhi Dhingra <er.surbhidhingra@gmail.com>
2015-09-26 16:42:14 +0530

    few changes

--- app/src/main/java/com/olxtechathon/olxmobiletechathon/UserDetailsActivity.java
+++ app/src/main/java/com/olxtechathon/olxmobiletechathon/UserDetailsActivity.java
@@ -1,12 +1,18 @@
 package com.olxtechathon.olxmobiletechathon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class UserDetailsActivity extends AppCompatActivity {
 
@@ -15,13 +21,37 @@ public class UserDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
 
-        Button btnnext = (Button)findViewById(R.id.btn_next);
+        Button btnnext = (Button) findViewById(R.id.btn_next);
         btnnext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(UserDetailsActivity.this, "Your data has been submitted", Toast.LENGTH_LONG).show();
             }
         });
+
+        String possibleEmail = null;
+        Pattern emailPattern = Patterns.EMAIL_ADDRESS; // API level 8+
+        Account[] accounts = AccountManager.get(UserDetailsActivity.this).getAccounts();
+        for (Account account : accounts) {
+            if (emailPattern.matcher(account.name).matches()) {
+                possibleEmail = account.name;
+                break;
+            }
+        }
+
+        EditText txtemail = (EditText) findViewById(R.id.txt_email);
+        if(possibleEmail != null){
+            txtemail.setText(possibleEmail);
+        }
+
+//        TelephonyManager tMgr = (TelephonyManager)getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+//        String mPhoneNumber = tMgr.getLine1Number();
+//
+//        EditText txtPhone = (EditText) findViewById(R.id.txt_phone);
+//        if(mPhoneNumber != null){
+//            txtPhone.setText(mPhoneNumber);
+//        }
+
     }
 
     @Override
